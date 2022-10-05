5301224845baab9ce083153c50bbfd26de22d119
VuseKaylous <vuvietkhanh1407@gmail.com>
2022-10-05 14:27:24 +0700

    update brick.java

--- src/main/java/com/example/project2/entities/Brick.java
+++ src/main/java/com/example/project2/entities/Brick.java
@@ -4,12 +4,26 @@ import com.example.project2.entities.Entity;
 import javafx.scene.image.Image;
 
 public class Brick extends Entity{
+    private boolean destroyed;
+    private int frame;
     public Brick(int x, int y, Image img) {
         super(x, y, img);
+        destroyed = false;
+        frame = 0;
+    }
+
+    public void setDestroyed() {
+        destroyed = true;
     }
 
     @Override
     public void update() {
-
+        if (destroyed) {
+            if (frame == 32 * 4) {
+                return;
+            }
+            frame = frame + 1;
+            this.img = Picture.brick[frame/32].getFxImage();
+        }
     }
 }
